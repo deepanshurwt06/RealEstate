2d59668410fb87116931713c2fb2082fb456f42d
deepanshurwt06 <rwtd06@gmail.com>
2025-03-21 13:58:12 +0530

    created user test route and change to es module

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js"; // Ensure file extension is included
+
 dotenv.config();
 const app = express();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
   })
@@ -13,7 +15,13 @@ mongoose
     console.log(err);
   });
 
-  
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.get("/", (req, res) => {  
+  res.send("Hello World");
+});
+
+// Use the imported user routes
+app.use("/api/user", userRoutes);
