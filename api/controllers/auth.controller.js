b0c8960ecfe9e76c6729d34a34b1a5adcc8dae3f
deepanshurwt06 <rwtd06@gmail.com>
2025-03-21 23:45:12 +0530

    create sign in api and route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,42 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
-import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+export const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
 
-export const signUp =async (req,res,next)=>{
-   const {username,email,password} = req.body;
-   const hashedPassword = bcrypt.hashSync(password,10);
-    const newUser = new User({username,email,password:hashedPassword});
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
 
-    try{
-        await newUser.save();
-        res.status(201).json({message:"User created successfully"});
-    }catch(err){
-      next(err);
+//sign in controller
+
+export const signin = async (req,res,next) =>{
+  const {email,password} = req.body;
+  try{
+    const validUser = await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404, 'User not found!'));
     }
+    const validPassword = bcrypt.compareSync(password,validUser.password);
 
-   
+    if(!validPassword) return next(errorHandler(401,'Invalid Email or Password!'));
+    
+    const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+    const {password: pass,...rest} = validUser._doc;
+    res.cookie('access_token',token,{httpOnly:true})
+    .status(200)
+    .json(rest)
+    
 
-
+  }catch(error){
+    next(error);
+  }
 }
