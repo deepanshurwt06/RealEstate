2fd1c07781b82a86fd5a95e5430ab14c98dee8f2
deepanshurwt06 <rwtd06@gmail.com>
2025-03-21 12:49:08 +0530

    set up mongo db for project

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-
-
-
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+  
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
