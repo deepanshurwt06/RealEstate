14f1c80a8c3190b918fb806e2deab946eebcc8ec
deepanshurwt06 <rwtd06@gmail.com>
2025-03-21 22:58:09 +0530

    complete sign up page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,25 +1,91 @@
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 export default function Signup() {
-    return (
-       <div className="p-5 max-w-lg mx-auto">
-        <h1 className="text-3xl font-semibold my-7 text-center">Sign Up</h1>
-        <form className="flex flex-col gap-4">
-            <input className="border bg-white border-zinc-300 p-3 rounded-lg " type="text" placeholder="username"
-            id="username"/>
-             <input className="border bg-white border-zinc-300 p-3 rounded-lg " type="email" placeholder="example@email.com"
-            id="email"/>
-             <input className="border bg-white border-zinc-300 p-3 rounded-lg " type="password" placeholder="password"
-            id="password"/>
-            <button className="bg-slate-700 text-white p-3 rounded-lg font-semibold uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-        </form>
-        <div className='flex gap-2 py-3 justify-center'>
-            <p className='font-medium'>Have an account?</p>
-            <Link to={'/sign-in'}>
-               <span className='text-blue-700 font-semibold'>Sign in</span>
-            </Link>
-        </div>
-       </div>
-    )
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+   
+    e.preventDefault();
+
+    try{
+        setLoading(true);
+        const res = await fetch("/api/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+    
+        if (data.success === false) {
+          setLoading(false);
+          setError(data.message);
+    
+          return;
+        }
+        setLoading(false);
+       
+        setError(null)
+       navigate('/sign-in')
+    }catch(error){
+        setLoading(false)
+        setError(error.message)
+    }
+
+   
+  };
+  console.log(formData);
+  return (
+    <div className="p-5 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold my-7 text-center">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          onChange={handleChange}
+          className="border bg-white border-zinc-300 p-3 rounded-lg "
+          type="text"
+          placeholder="username"
+          name="username"
+        />
+        <input
+          onChange={handleChange}
+          className="border bg-white border-zinc-300 p-3 rounded-lg "
+          type="email"
+          placeholder="example@email.com"
+          name="email"
+        />
+        <input
+          onChange={handleChange}
+          className="border bg-white border-zinc-300 p-3 rounded-lg "
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg font-semibold uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-2 py-3 justify-center">
+        <p className="font-medium">Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700 font-semibold">Sign in</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
