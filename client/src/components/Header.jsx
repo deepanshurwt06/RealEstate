dc01b1ba21e7dd420ec1032f53682086f7a5dbed
deepanshurwt06 <rwtd06@gmail.com>
2025-03-24 15:22:41 +0530

    fixed the profile picture  problem

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+ 
   return (
     <header className="bg-slate-200">
       <div className="flex p-5 justify-between items-center max-w-[97rem] mx-auto">
@@ -35,7 +36,7 @@ export default function Header() {
 
           <Link to="/profile">
             {currentUser ? (
-              <img className="rounded-full h-7 w-7 object-cover" src="{currentUser.avatar}" alt="profile" />
+              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
             ) : (
               <li className=" text-slate-700 hover:underline">Sign in</li>
             )}
